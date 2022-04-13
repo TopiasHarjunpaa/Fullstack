9cfd27d07234b911c2b8731756726d4b2ff2045f
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-04-13 23:19:53 +0300

    exercise 4.2

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,28 +1,11 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-
-
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const mongoose = require('mongoose')
-const url = config.MONGODB_URI
-
-mongoose.connect(url).then(() => {
-  logger.info('connected to MongoDB')
-})
-  .catch((error => {
-    logger.error('error connecting to MongoDB', error.message)
-  }))
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+const server = http.createServer(app)
 
 const PORT = config.PORT | 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
