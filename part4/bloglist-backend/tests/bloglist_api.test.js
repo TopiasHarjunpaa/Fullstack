3092cf077352bf7bbc9680c36a6e0bbbf20fbbdb
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-04-19 22:55:59 +0300

    exercises 4.10-4.12

--- part4/bloglist-backend/tests/bloglist_api.test.js
+++ part4/bloglist-backend/tests/bloglist_api.test.js
@@ -29,12 +29,12 @@ test('blogs should have indentifying field named as id', async () => {
   expect(ids).toBeDefined()
 })
 
-test('new blog can be added with correct title', async () => {
+test('new blog can be added with correct title and likes', async () => {
   const newBlog = {
     title: 'For testing',
     author: 'Tester',
     url: 'test.com',
-    likes: 1
+    likes: 5
   }
 
   await api
@@ -45,9 +45,61 @@ test('new blog can be added with correct title', async () => {
 
   const blogsAtEnd = await helper.blogsInDb()
   const titles = blogsAtEnd.map(b => b.title)
+  const likes = blogsAtEnd.filter(b => b.title === newBlog.title).map(b => b.likes)
   expect(blogsAtEnd).toHaveLength(helper.Initialblogs.length + 1)
   expect(titles).toContainEqual('For testing')
+  expect(likes).toContain(newBlog.likes)
 
+})
+
+test('blog without likes input gets value of 0', async () => {
+  const newBlog = {
+    title: 'Without likes',
+    author: 'No likes',
+    url: 'likes.net'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const likes = blogsAtEnd.filter(b => b.title === newBlog.title).map(b => b.likes)
+  expect(blogsAtEnd).toHaveLength(helper.Initialblogs.length + 1)
+  expect(likes).toContain(0)
+})
+
+test('adding blog without title does not success', async () => {
+  const newBlog = {
+    author: 'No title',
+    url: 'notitle.net',
+    likes: 3
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.Initialblogs.length)
+})
+
+test('adding blog without url does not success', async () => {
+  const newBlog = {
+    title: 'No url',
+    author: 'nourl'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.Initialblogs.length)
 })
 
 afterAll(() => {
