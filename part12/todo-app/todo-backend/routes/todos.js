0f3b9bc66cfda6e550399018a957b9b77d759fda
Topias Harjunpää <topias.harjunpaa@gmail.com>
2025-01-07 22:26:17 +0200

    exercise 12.7

--- part12/todo-app/todo-backend/routes/todos.js
+++ part12/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,19 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body
+
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    req.params.id,
+    { text, done },
+    { new: true, runValidators: true, context: 'query' }
+  )
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
